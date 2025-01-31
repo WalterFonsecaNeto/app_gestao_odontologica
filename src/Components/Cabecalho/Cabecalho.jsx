60db9b57f97a345f9e391443c619ac425b6e429b
Walter Fonseca Neto <walterfonseca1606@gmail.com>
2025-01-31 00:51:45 -0300

    Criação do menuHamburguer

--- src/Components/Cabecalho/Cabecalho.jsx
+++ src/Components/Cabecalho/Cabecalho.jsx
@@ -1,17 +1,39 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import style from "./Cabecalho.module.css";
 import { IoIosArrowDown } from "react-icons/io";
+
+import MenuHamburguer from "../MenuHamburguer/MenuHamburguer";
+
 function Cabecalho() {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIsAnimating((prev) => !prev); // Alterna o estado a cada 10 segundos
+    }, 3000);
+
+    return () => clearInterval(intervalId); // Limpeza do intervalo ao desmontar o componente
+  }, []);
+
   return (
     <header className={style.cabecalho}>
-      <Link to="/home">
-        <button className={style.button} data-text="Awesome">
-          <span className={style.actual_text}>&nbsp;OdontoSync&nbsp;</span>
-          <span aria-hidden="true" className={style.hover_text}>
-            &nbsp;OdontoSync&nbsp;
-          </span>
-        </button>
-      </Link>
+      <div className={style.container_menu_logo}>
+        <div className={style.menuHamburguerContainer}>
+          <MenuHamburguer />
+        </div>
+        <Link to="/home">
+          <button className={style.button} data-text="Awesome">
+            <span className={style.actual_text}>&nbsp;OdontoSync&nbsp;</span>
+            <span
+              aria-hidden="true"
+              className={`${style.hover_text} ${isAnimating ? style.animate : ""}`}
+            >
+              &nbsp;OdontoSync&nbsp;
+            </span>
+          </button>
+        </Link>
+      </div>
       <div className={style.menu_total}>
         <div className={style.menu}>
           <div className={style.item}>
